9432cddb47ba66cde80c20aa78775ee1632bdbf8
Dmitri Gorun <iii448@mail.ru>
2022-02-17 22:00:13 +0500

    third commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 3,
+  centeredSlides: true,
+  loopedSlides: 4,
+
+});
+
+sliderThumbs.on('click', (swiper) => {
+  swiper.slideTo(swiper.clickedIndex);
+});
+
+const sliderMain = new Swiper('.slider-main', {
+
+  loop: true,
+  spaceBetween: 10,
+  loopedSlides: 4,
+  // thumbs: {
+  //   swiper: sliderThumbs,
+  // },
+
+});
+
+sliderThumbs.controller.control = sliderMain;
+sliderMain.controller.control = sliderThumbs;
